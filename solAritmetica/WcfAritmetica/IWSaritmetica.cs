f331699de07462ff37ca94b53f82cf943d8a735f
pcloayza <paulocesarloayza@upsa.edu.bo>
2019-03-02 10:07:56 -0400

    segundo commit

---
+++ solAritmetica/WcfAritmetica/IWSaritmetica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfAritmetica
+{
+   
+    [ServiceContract]
+    public interface IWSaritmetica
+    {
+        [OperationContract]
+        clsAritmetica sumar(int numero1, int numero2);
+    }
+
+    [DataContract]
+    public class clsAritmetica
+    {
+        [DataMember]
+        public int numeroA { get; set; }
+        [DataMember]
+        public int numeroB { get; set; }
+        [DataMember]
+        public int respuesta { get; set; }
+
+    }
+}
---
+++ solAritmetica/WcfAritmetica/WSaritmetica.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfAritmetica
+{
+    public class WSaritmetica : IWSaritmetica
+    {
+        public clsAritmetica sumar(int numero1, int numero2)
+        {
+            clsAritmetica obj = new clsAritmetica();
+            obj.numeroA = numero1;
+            obj.numeroB = numero2;
+            obj.respuesta = obj.numeroA + obj.numeroB;
+
+            return obj;
+        }
+    }
+}
